74b644db4224eb0206574b82f9a282660cab4255
Szymon <szymi17s@interia.pl>
2025-03-13 12:56:29 +0100

    PRM test

--- Lab1/Zad1.m
+++ Lab1/Zad1.m
@@ -2,7 +2,7 @@ clear
 clc
 
 %Tworzenie siatki
-myMap = binaryOccupancyMap(100,100,10 ,"grid");
+map = binaryOccupancyMap(100,100,10 ,"grid");
 
 %Tworzenie ścian
 walls = zeros(100,100);
@@ -19,6 +19,46 @@ walls(60:100,75) = 1;
 walls(35,75:100) = 1; 
 walls(90:100,50) = 1; 
 
-setOccupancy(myMap,[1 1],walls,"grid")
-show(myMap)
+setOccupancy(map,[1 1],walls,"grid")
+% show(map)
 
+ss = stateSpaceSE2;
+ss.StateBounds = [map.XWorldLimits; map.YWorldLimits; [-pi pi]];
+
+sv = validatorOccupancyMap(ss,Map=map);
+sv.ValidationDistance = 0.01;
+
+planner = plannerPRM(ss,sv); % tutaj wybierasz funkcje  plannerPRM(ss,sv, "MaxConnectionDistance", 1, "MaxNumNodes", 200)
+
+graph = graphData(planner);
+
+edges = table2array(graph.Edges);
+nodes = table2array(graph.Nodes);
+
+start = [10 10 0];
+goal = [90 90 0];
+
+show(sv.Map)
+hold on
+plot(nodes(:,1),nodes(:,2),"*","Color","b","LineWidth",2)
+for i = 1:size(edges,1)
+    % Samples states at distance 0.02 meters.
+    states = interpolate(ss,nodes(edges(i,1),:), ...
+                         nodes(edges(i,2),:),0:0.02:1);
+    plot(states(:,1),states(:,2),"Color","b")
+end
+plot(start(1),start(2),"*","Color","g","LineWidth",3)
+plot(goal(1),goal(2),"*","Color","r","LineWidth",3)
+
+
+rng(100,"twister");
+[pthObj, solnInfo] = plan(planner,start,goal);
+
+if solnInfo.IsPathFound
+    interpolate(pthObj,1000);
+    plot(pthObj.States(:,1),pthObj.States(:,2), ...
+         "Color",[0.85 0.325 0.098],"LineWidth",2)
+else
+    disp("Path not found")
+end
+hold off
