d34412de514e915e5025ee1186ce9e51e2827cab
Yulekk13 <piotrekm2001@interia.eu>
2025-03-13 10:38:48 +0100

    Trasa

--- Lab1/Zad1.m
+++ Lab1/Zad1.m
@@ -1,34 +1,24 @@
+clear 
 clc
-close all
 
+%Tworzenie siatki
 myMap = binaryOccupancyMap(100,100,10 ,"grid");
-%with properties:
 
-   % mapLayer Properties
-   %            LayerName: 'binaryLayer'
-   %             DataType: 'logical'
-   %         DefaultValue: 0
-   %  GridLocationInWorld: [0 0]
-   %    GridOriginInLocal: [0 0]
-   %   LocalOriginInWorld: [0 0]
-   %           Resolution: 5
-   %             GridSize: [50 50]
-   %         XLocalLimits: [0 10]
-   %         YLocalLimits: [0 10]
-   %         XWorldLimits: [0 10]
-   %         YWorldLimits: [0 10]
+%Tworzenie ścian
+walls = zeros(100,100);
+walls(1,:) = 1; % Górna ściana
+walls(end,:) = 1; % Dolna ściana
+walls(:,1) = 1; % Lewa ściana
+walls(:,end) = 1; % Prawa ściana
 
-walls = zeros(50,50);
-walls(1,:) = 1; % Top wall
-walls(end,:) = 1; % Bottom wall
-walls(:,1) = 1; % Left wall
-walls(:,end) = 1; % Right wall
-walls(1:30,15) = 1; % Left division
-walls(25:end,25) = 1; % Middle division
-walls(1:30,35) = 1; % Right division
-
-walls(8,1:4)=1;
-
+%Ściany wewnętrzne
+walls(1:40, 25) = 1;
+walls(1:60,50) = 1;
+walls(70,15:35) = 1; 
+walls(60:100,75) = 1; 
+walls(35,75:100) = 1; 
+walls(90:100,50) = 1; 
 
 setOccupancy(myMap,[1 1],walls,"grid")
 show(myMap)
+
